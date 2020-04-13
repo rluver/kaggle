28105ed282459fc60726a30a12292d6feb32492a
Jihwan Min <55495983+rluver@users.noreply.github.com>
2020-04-13 15:44:12 +0900

    refactor: Refactor transformers_albert

--- NLP with Disaster Tweets/transforemrs_albert.py
+++ NLP with Disaster Tweets/transforemrs_albert.py
@@ -1,77 +1,82 @@
-import pandas as pd
+import os
+os.environ['TF_KERAS'] = '1'
+
 import numpy as np
-import tensorflow as tf
-import tensorflow.keras.backend as K
-from sklearn.model_selection import KFold, GroupKFold
+import pandas as pd
 from transformers import *
-from tensorflow.keras.layers import Input, Dense, Dropout
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Input
 from tensorflow.keras.preprocessing import sequence
-from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Nadam
+from tensorflow.keras.models import Model
 
+# load data
+train = pd.read_csv('d:/nlp-getting-started/train.csv')
+test = pd.read_csv('d:/nlp-getting-started/test.csv')
+submission = pd.read_csv('d:/nlp-getting-started/sample_submission.csv')
 
+# set parameter
+MAX_LEN = 70
+EPOCH = 3
+BATCH_SIZE = 16
 
-from tensorflow.python.client import device_lib
-print(device_lib.list_local_devices())
-
-
-
-# data load
-train = pd.read_csv('D:/nlp-getting-started/train.csv')
-test = pd.read_csv('D:/nlp-getting-started/test.csv')
-submission = pd.read_csv('D:/nlp-getting-started/sample_submission.csv')
-
-
+# load albert
 tokenizer = AlbertTokenizer.from_pretrained('albert-xlarge-v2', do_lower_case = True)
-model_albert = TFAlbertModel.from_pretrained('albert-xlarge-v2')
+albert = TFAlbertModel.from_pretrained('albert-xlarge-v2')
 
 
-# parameter
-seq_len = 50
+# user define function
+def encode(self, tokenizer, max_len = 100):
+       
+    text = self.text.apply(lambda x: tokenizer.tokenize(x))
+    text = text.apply(lambda x: x[:68])
+    text = text.apply(lambda x: ['[CLS]'] + x + ['[SEP]'])
+    text = text.apply(lambda x: tokenizer.convert_tokens_to_ids(x))
+    
+    token = sequence.pad_sequences(text, maxlen = MAX_LEN, padding = 'post', dtype = 'int32')
+    length = text.apply(lambda x: len(x))
+    mask = list(map(lambda x: [1] * x + [0] * (MAX_LEN - x), length))
+    seg_id = [[0] * MAX_LEN] * self.shape[0]
+       
+    return np.array(token), np.array(mask), np.array(seg_id)
 
-def preprocess():
-    
-    train['tokenized'] = train.text.apply(lambda x: tokenizer.encode(x, max_length = seq_len))
-    
-    return(sequence.pad_sequences(train.tokenized, maxlen = seq_len, padding = 'post'))
-    
 
-def modelBuild():
+def build_model(model, max_len = 100, num_labels = 1):
     
-    input = Input((seq_len), dtype = tf.int32, name = 'input_token')    
-    layer1, _ = model_albert(input)
-    mean_pooling = GlobalAveragePooling1D()(layer1)
-    max_pooling = GlobalMaxPooling1D()(layer1)
-    pooling = concatenate([mean_pooling, max_pooling])
-    drop1 = Dropout(0.5,
-                    name = 'drop1')(pooling)
-    layer2 = Dense(units = 1024,
-                   activation = 'selu',
-                   name = 'layer2')(drop1)               
-    drop2 = Dropout(0.5,
-                    name = 'drop2')(layer2)
-    output = Dense(units = 1, 
-                   activation = 'sigmoid',
-                   kernel_initializer = glorot_normal(),
-                   bias_initializer = glorot_normal(),
-                   name = 'output')(drop2)
+    input_word = Input(shape = (max_len, ), dtype = tf.int32, name = 'input_word')
+    input_mask = Input(shape = (max_len, ), dtype = tf.int32, name = 'input_mask')
+    input_seg_id = Input(shape = (max_len, ), dtype = tf.int32, name = 'input_seg_id')
 
+    input_ = [input_word, input_mask, input_seg_id]        
+    _, temp = model([input_word, input_mask, input_seg_id])    
+    temp1 = Dense(units = 512,
+                  name = 'temp_layer1')(temp)
+    output_ = Dense(units = num_labels,
+                    name = 'output')(temp1)
     
-    model = Model(input, output)
-    print(model.summary())
-    model.compile(optimizer = Nadam(learning_rate = 0.0001),
-                  metrics = 'accuracy',
-                  loss = 'binary_crossentropy')
+    model = Model(inputs = input_,
+                  outputs = output_)
+    model.compile(optimizer =Nadam(lr = 3e-5),
+                  loss = 'binary_crossentropy',
+                  metrics = ['accuracy'])
     
     return model
 
 
-train_tokenized = preprocess()
 
-model = modelBuild()
+# preprocess
+train_ = encode(train, tokenizer, max_len = MAX_LEN)
+test_ = encode(test, tokenizer, max_len = MAX_LEN)
 
-model.fit(train_tokenized,
-          np.array(train.target),
-          batch_size = 4,
-          epochs = 3,
-          validation_split = 0.3)
+# model
+model = build_model(model = albert,
+                    max_len = MAX_LEN,
+                    num_labels = 1)
+
+# train
+history = model.fit(
+    train_,
+    np.array(train.target),
+    validation_split = 0.3,
+    epochs = EPOCH,
+    batch_size = BATCH_SIZE)
